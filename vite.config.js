b22e0ff7ac3979cbbc7a3e0719dbd6139e2da2d0
Cristian Vogel <cristian.vogel55@gmail.com>
2024-01-24 19:06:25 +0100

    Moving my submodule

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
 
 import { execSync } from 'node:child_process'
 
@@ -37,5 +37,5 @@ export default defineConfig({
     __COMMIT_HASH__: JSON.stringify(currentCommit),
     __BUILD_DATE__: JSON.stringify(dateString),
   },
-  plugins: [react(), pubDirReloadPlugin()],
+  plugins: [ pubDirReloadPlugin() ],
 })
