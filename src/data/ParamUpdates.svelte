e969e1a07662f257fe6dcebde8e7af3aa6ebf116
Cristian Vogel <cristian.vogel55@gmail.com>
2024-02-22 13:13:07 +0100

    added useful comments

--- src/data/ParamUpdates.svelte
+++ src/data/ParamUpdates.svelte
@@ -24,7 +24,6 @@
         if (!$HostState) break;
         const cablesVarName = "param_" + pid;
         try {
-          //  console.log("param -> " + pid);
           const parsedHostState = $HostState;
           if ($UpdateStateFSM === "updatingUI") {
             $CablesPatch.setVariable(cablesVarName, parsedHostState[pid]);
@@ -34,27 +33,31 @@
     }
   }
 
-  function getParamIDsFromCablesVars(): string[] {
-    return Object.keys($CablesParams)
-      .filter((k) => k.startsWith("param_") && k !== "param_pickedID")
-      .map((k) => k.replace("param_", ""));
-  }
+  onMount(() => {
+    // first, construct the array of FSMs and put them in a Writable
+    statesArrayStoreInit();
+  });
 
   let viewState: number[];
+
+  // important reactive store here, will either initialise empty state
+  // or restore from host viewState
   $: viewState = $HostState.viewState || new Array(36).fill(0);
 
   function statesArrayStoreInit() {
+    // we are going to make an array of Finite State Machines to
+    // hold the states of each Node in the UI
     let arr: FSM[] = new Array(manifest.NUMBER_NODES).fill(null);
-    // HostState.viewState is never ready here
     for (let i = 0; i < manifest.NUMBER_NODES; i++) {
       const startingState: number = viewState ? viewState[i] : 0;
       arr[i] = createNodeStateFSM(
         ["empty", "filled"].at(startingState) as "empty" | "filled" | undefined
       );
     }
-
+    // make sure the FSMs are put in a Writable before accessing
     $UI_StateArrayFSMs = arr;
 
+    // derive a 'parsed' version of the FSMs result ( "empty" | "filled" ) as a numerical array
     $UI_ParsedStates.parsed = () => {
       return $UI_StateArrayFSMs.map((state: any) => {
         return get(state) === "empty" ? 0 : 1;
@@ -62,7 +65,10 @@
     };
   }
 
-  onMount(() => {
-    statesArrayStoreInit();
-  });
+  // utility for renaming params from Cables
+  function getParamIDsFromCablesVars(): string[] {
+    return Object.keys($CablesParams)
+      .filter((k) => k.startsWith("param_") && k !== "param_pickedID")
+      .map((k) => k.replace("param_", ""));
+  }
 </script>
